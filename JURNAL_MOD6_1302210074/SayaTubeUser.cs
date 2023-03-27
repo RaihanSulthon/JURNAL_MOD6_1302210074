64dac2368e48591d870b933570f38c101db9f6ea
PC4-IFLAB <PC4-IFLAB>
2023-03-27 07:48:06 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ JURNAL_MOD6_1302210074/SayaTubeUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JURNAL_MOD6_1302210074
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        String username;
+
+        public SayaTubeUser(String username)
+        {
+            Random random = new Random();
+            this.id = random.Next(1, 99999);
+            this.uploadedVideos= new List<SayaTubeVideo>();
+            this.username = username;
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int total = 0;
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                total = total + uploadedVideos[i].GetPlayCount();
+            }
+            return total;
+        }
+
+        public void AddVideo(SayaTubeVideo a)
+        {
+            uploadedVideos.Add(a);
+        }
+
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine(" User :" + username);
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                Console.WriteLine("Video " + (i+1)+ " Judul : " + uploadedVideos[i].GetTitle());
+            }
+        }
+    }
+}
---
+++ JURNAL_MOD6_1302210074/SayaTubeVideo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JURNAL_MOD6_1302210074
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            Random random = new Random();
+            this.id = random.Next(1, 99999);
+            if(title == null)
+            {
+                throw new NullReferenceException("Judul tidak boleh kosong");
+            }
+            if(title.Length > 99)
+            {
+                throw new Exception("Maximal panjang dari teks adalah 100");
+                this.title = title;
+            }
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int x)
+        {
+            this.playCount = x;
+        }
+
+        public int GetPlayCount()
+        {
+            return this.playCount;
+        }
+
+        public String GetTitle()
+        {
+            return this.title;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID :" + id);
+            Console.WriteLine("Title :" + title);
+            Console.WriteLine("PlayCount : " + (playCount+1));
+        }
+    }
+}
